157644ca769fa10b051c9ef873c90b9be5005451
ahmed nasser <ahmednas197@gmail.com>
2023-09-26 13:15:13 +0300

    Breadth first Search on Binary tree

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,17 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    let q: BinaryNode<number>[] | null | undefined = [head];
 
+    while (q.length) {
+        let curr = q.shift() as BinaryNode<number>;
+        if (curr.value === needle) {
+            return true;
+        }
+        if (curr.left) {
+            q.push(curr.left);
+        }
+        if (curr.right) {
+            q.push(curr.right);
+        }
+    }
+    return false;
 }
